599f0543c389ee6b05f6dde3c83438ca58071e54
zgq <>
2021-07-21 20:18:21 +0800

    feature f1 test4 commit

--- app/src/main/java/com/example/test/F2.kt
+++ app/src/main/java/com/example/test/F2.kt
@@ -26,4 +26,8 @@ class F2 :Fragment(){
     fun test3(){
         System.out.println("feature f1 test3 commit")
     }
+
+    fun test4(){
+        System.out.println("feature f1 test4 commit")
+    }
 }
